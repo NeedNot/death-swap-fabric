2e37fda7ad663e231d61cc7f4c2a705458fe051f
NeedNot <daltonhornor0@gmail.com>
2020-07-24 11:10:25 -0700

    what ever

--- main.py
+++ main.py
@@ -1,14 +1,32 @@
 from PIL import Image, ImageDraw, ImageFont
 import os
 
+#dice
+dice = input("Dice total:")
+
+#color
+color = input("Color:")
+
+#card
+card = input("Card:")
+
+#number
+number = input("Number:")
+
+#battery
+battery = input("Battery:")
+
+#page
+page = "Dice total:\n" + dice + "\n\n" + "Color:\n" + color +"\n\n" + "Card:\n" + card +"\n\n" + "Number:\n" + number +"\n\n" + "Battery:\n" + battery +"\n\n"
+
 # name of the file to save
-filename = "image.png"
+filename = "image.jpeg"
 fnt = ImageFont.truetype('arial.ttf', 12)
 
 # create new image
-image = Image.new(mode = "RGB", size = (200,70), color = "red")
+image = Image.new(mode = "RGB", size = (497,275), color = "white")
 draw = ImageDraw.Draw(image)
-draw.text((10,10), "My Text", font=fnt, fill=(255,255,0))
+draw.text((2,10), page, font=fnt, fill=(0,0,0))
 image.save(filename)
 
 # save the file
